5cbe455d4664eb55190ee978fbabe85c28777371
Max Harmathy <max.harmathy@web.de>
2020-05-06 16:50:14 +0200

    Sanitize run.sh script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -8,14 +8,20 @@ cd /tmp/pkg
 
 # Install (official repo + AUR) dependencies using yay. We avoid using makepkg
 # -s since it is unable to install AUR dependencies.
-yay -Sy --noconfirm \
-    $(pacman --deptest $(source ./PKGBUILD && echo ${depends[@]} ${makedepends[@]}))
+raw_info="$(makepkg --printsrcinfo)"
+printf "%s\n" "$raw_info" | grep -F depends | cut -f 2 -d '=' | sort -u > depends.txt
+printf "%s\n" "$raw_info" | grep -F pkgname | cut -f 2 -d '=' | sort    > pgkname.txt
+deps="$(comm -23 depends.txt pgkname.txt)"
+
+# here we want word splitting 
+# shellcheck disable=SC2046,SC2086
+yay -Sy --noconfirm $(pacman --deptest $deps)
 
 # Do the actual building
 makepkg -f
 
 # Store the built package(s). Ensure permissions match the original PKGBUILD.
 if [ -n "$EXPORT_PKG" ]; then
-    sudo chown $(stat -c '%u:%g' /pkg/PKGBUILD) ./*.pkg.tar.xz
+    sudo chown "$(stat -c '%u:%g' /pkg/PKGBUILD)" ./*.pkg.tar.xz
     sudo mv ./*.pkg.tar.xz /pkg
 fi
