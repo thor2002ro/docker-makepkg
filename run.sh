e6196bd22bb7fcd69efbde4aa4d4a1228af158f7
Max Harmathy <max.harmathy@web.de>
2020-07-07 17:08:36 +0200

    Don't cut off version restrictions
    e.g. depends = linux>=5.7.7

--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ cd /tmp/pkg
 # Install (official repo + AUR) dependencies using yay. We avoid using makepkg
 # -s since it is unable to install AUR dependencies.
 raw_info="$(makepkg --printsrcinfo)"
-printf "%s\n" "$raw_info" | grep -F depends | cut -f 2 -d '=' | sort -u > depends.txt
-printf "%s\n" "$raw_info" | grep -F pkgname | cut -f 2 -d '=' | sort    > pgkname.txt
+printf "%s\n" "$raw_info" | grep -F depends | cut -f 2- -d '=' | sort -u > depends.txt
+printf "%s\n" "$raw_info" | grep -F pkgname | cut -f 2- -d '=' | sort    > pgkname.txt
 deps="$(comm -23 depends.txt pgkname.txt)"
 
-# here we want word splitting 
+# here we want word splitting
 # shellcheck disable=SC2046,SC2086
 yay -Sy --noconfirm $(pacman --deptest $deps)
 
